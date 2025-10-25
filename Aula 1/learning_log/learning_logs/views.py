dafa36228c562b4bac5c32aefb915fc271dd386a
Kelvin-Cuinica <kelvincuinica@gmail.com>
2025-10-25 16:34:03 +0200

    Criação da página topics

--- Aula 1/learning_log/learning_logs/views.py
+++ Aula 1/learning_log/learning_logs/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
 def index(request):
     """Irá exibir a pagina inicial do site Learning Logs"""
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """Ira exibir a pagina dos Topicos"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics' : topics}
+    return render(request, 'learning_logs/topics.html', context)
